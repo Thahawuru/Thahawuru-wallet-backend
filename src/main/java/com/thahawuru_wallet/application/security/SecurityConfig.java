84c31fb1fe7317c17c290bb545dc3b10d4fba646
Sonal Induwara <sonalinduwara@gmail.com>
2024-07-20 19:13:08 +0530

    minor

--- src/main/java/com/thahawuru_wallet/application/security/SecurityConfig.java
+++ src/main/java/com/thahawuru_wallet/application/security/SecurityConfig.java
@@ -43,7 +43,7 @@ public class SecurityConfig {
 
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:8081","http://localhost:8082")); // Replace with your Expo app's origin
+        configuration.setAllowedOrigins(List.of("http://localhost:8081","http://localhost:8082"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
